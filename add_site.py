b289a397b7f677607caf89ef886c059223e6825e
Kunal Pandit <119098582+Kunal448@users.noreply.github.com>
2025-10-16 15:17:56 +0530

    add_site.py

--- add_site.py
+++ add_site.py
@@ -4,14 +4,17 @@ from streamlit_gsheets import GSheetsConnection
 
 def page1():
     st.title("🏗️ Add New Site Page")
-    st.write("Welcome!")
+    st.write("Add a new construction site!")
     
     url = "https://docs.google.com/spreadsheets/d/1OliyAKJqz_A2NNVcRBdEpkqnUrnGOLaWsAD1niC9F9U/edit?usp=sharing"
     conn = st.connection("gsheets", type = GSheetsConnection)
     
-    data = conn.read(worksheet="Sheet1", ttl=0)
+    data = conn.read(worksheet="Sheet1", ttl=2)
     df = pd.DataFrame(data)
     max_site = df['Sheet'].max()  
+    st.write(f"Current number of sites: {'&nbsp;'*1} {int(max_site)}")
+    st.write("")
+    st.write("")
     site, _ = st.columns([1, 1])
     with site:
         site1 = st.text_input("Site Name", str(), placeholder="Type site name here...")
@@ -20,7 +23,7 @@ def page1():
     save_site = st.button("Add Site")
         
     if save_site:
-        existing_data = conn.read(worksheet="Sheet1", usecols=list(range(2)), ttl=0)
+        existing_data = conn.read(worksheet="Sheet1", usecols=list(range(2)), ttl=2)
         existing_data = existing_data.dropna(how="all")            
         result_data = pd.DataFrame(
             [
@@ -32,5 +35,4 @@ def page1():
         )        
         updated_df = pd.concat([existing_data, result_data], ignore_index=True)    
         conn.update(worksheet="Sheet1", data=updated_df)        
-
         st.success("New Site Saved Successfully")
